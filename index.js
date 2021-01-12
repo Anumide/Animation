6bdd6b459edb8d465f98260775c1b204ed835531
Anumide <alabasamuelayomide3@gmail.com>
2021-01-12 22:36:43 +0100

    Added javaScript

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-
+window.onload = ()=>{
     const colors = ["rgb(81, 99, 122)", "rgb(116, 91, 87)", "rgb(240, 240, 240)", "rgb(204, 204, 204)"];
     btn = document.querySelector('#btn');
     svgs = document.querySelectorAll('svg')
     countdown: 0;
     
-    btn.addEventListener('click', e =>{
+    btn.addEventListener('click', () =>{
         svgs.forEach(svg =>{
-            let random = math.random() * 4;
+            let random = Math.trunc(Math.random() * 4);;
             svg.style.fill = colors[random];
-        })
+        });
     });
 
 
 
+}
 
